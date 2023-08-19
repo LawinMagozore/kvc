8d517bd15b398da620669c0c92fb98f7634a6c52
LawinMagozore <magozorel@gmail.com>
2023-08-19 02:04:11 +0200

    update

--- index.php
+++ index.php
@@ -29,23 +29,23 @@
 	<div class="clearfix"></div>
 </div>
 <div class="w3_navigation">
-	<div class="container-fluid">
+	<div class="container">
 		<nav class="navbar-brand navbar-default">
-			<div class="navbar-header navbar-right">
+			<div class="navbar navbar-right">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<h1><a class="navbar-brand" href="index.php">Kyle <span>View</span>
+				<h1><a class="navbar " href="index.php">Kyle <span>View</span>
 						<p class="logo_w3l_agile_caption">Your Dreamy Resort</p>
 					</a></h1>
 			</div>
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse navbar-left" id="bs-example-navbar-collapse-1">
 				<nav class="menu menu--iris ">
-					<ul class="nav navbar-nav  ">
+					<ul class="navbar-nav flex-row d-none d-md-flex">
 						<li class="menu__item menu__item--current"><a href="" class="menu__link">Home</a></li>
 						<li class="menu__item"><a href="#about" class="menu__link scroll">About</a></li>
 						<li class="menu__item"><a href="#team" class="menu__link scroll">Team</a></li>
