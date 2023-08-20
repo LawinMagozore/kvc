ba2a92ee31787f5d5cb35086e57d5512d51d221f
LawinMagozore <magozorel@gmail.com>
2023-08-20 11:34:39 +0200

    update

--- inquiries.php
+++ inquiries.php
@@ -1,6 +1,21 @@
 <?php include("header.php") ?>
 
+<section class="bg-dark vh-100 d-flex">
+    <div class="col-9 m-auto ">
+        <div class="card">
 
+            <div class="card-header">
+                <h3 class="card-title">You can Ask for a Qoutation here Fill in the above form </h3>
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+
+</section>
 
 
 <?php include("footer.php") ?>
