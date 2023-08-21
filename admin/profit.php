871a0294c62aadcb9efa0143b961587893674904
LawinMagozore <magozorel@gmail.com>
2023-08-22 01:57:34 +0200

    update

--- admin/profit.php
+++ admin/profit.php
@@ -1,38 +1,39 @@
-<?php  
-session_start();  
-if(!isset($_SESSION["user"]))
-{
- header("location:index.php");
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+    header("location:index.php");
 }
-?> 
+?>
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-      <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SUNRISE HOTEL</title>
-	<!-- Bootstrap Styles-->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-     <!-- FontAwesome Styles-->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    
-	<link rel="stylesheet" href="assets/css/morris.css">
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js//raphael-min.js"></script>
-	<script src="assets/js/morris.min.js"></script>
 
-   
-        <!-- Custom Styles-->
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>KYLE VIEW CHARLETS</title>
+    <!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="assets/css/morris.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js//raphael-min.js"></script>
+    <script src="assets/js/morris.min.js"></script>
+
+
+    <!-- Custom Styles-->
     <link href="assets/css/custom-styles.css" rel="stylesheet" />
-     <!-- Google Fonts-->
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-     <!-- TABLE STYLES-->
+    <!-- Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <!-- TABLE STYLES-->
     <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
 </head>
+
 <body>
     <div id="wrapper">
-        
+
         <nav class="navbar navbar-default top-navbar" role="navigation">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
@@ -72,186 +73,181 @@ if(!isset($_SESSION["user"]))
                         <a href="home.php"><i class="fa fa-dashboard"></i> Status</a>
                     </li>
                     <li>
-                        <a  href="messages.php"><i class="fa fa-desktop"></i> News Letters</a>
+                        <a href="messages.php"><i class="fa fa-desktop"></i> News Letters</a>
                     </li>
-					<li>
+                    <li>
                         <a href="roombook.php"><i class="fa fa-bar-chart-o"></i>Room Booking</a>
                     </li>
                     <li>
-                        <a  href="payment.php"><i class="fa fa-qrcode"></i> Payment</a>
+                        <a href="payment.php"><i class="fa fa-qrcode"></i> Payment</a>
                     </li>
-					 <li>
+                    <li>
                         <a class="active-menu" href="profit.php"><i class="fa fa-qrcode"></i> Profit</a>
                     </li>
-                    
-                    <li>
-                        <a href="logout.php" ><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                    </li>
-                    
 
-                    
+                    <li>
+                        <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                    </li>
+
+
+
             </div>
 
         </nav>
         <!-- /. NAV SIDE  -->
-        <div id="page-wrapper" >
+        <div id="page-wrapper">
             <div id="page-inner">
-			 <div class="row">
+                <div class="row">
                     <div class="col-md-12">
                         <h1 class="page-header">
-                           Profit Details<small> </small>
+                            Profit Details<small> </small>
                         </h1>
                     </div>
-                </div> 
-                 <!-- /. ROW  -->
-				 
-				 
-            <div class="row">
-			
-				<?php 
-				//index.php
-				//$connect = mysqli_connect("localhost", "root", "", "hotel");
-				include('db.php');
-				
-					
-					$query = "SELECT * FROM payment";
-					$result = mysqli_query($con, $query);
-					$chart_data = '';
-					$tot = 0;
-					while($row = mysqli_fetch_array($result))
-					{
-					 $chart_data .= "{ date:'".$row["cout"]."', profit:".$row["fintot"] *10/100 ."}, ";
-					 $tot = $tot + $row["fintot"] *10/100;
-					}
-					$chart_data = substr($chart_data, 0, -2);
-				
-?>
-				 
-				<br>
-				<br>
-				<br>
-				<br><div id="chart"></div>
-                <div class="col-md-12">
-                    <!-- Advanced Tables -->
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-											<th>ID</th>
-                                            <th>Name</th>
-                                            <th>Check in</th>
-											<th>Check out</th>
-                                            <th>Room Rent</th>
-											<th>Bed Rent</th>
-											<th>Meals </th>
-											<th>Gr.Total</th>
-											<th>Profit</th>
-											
-                                            
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        
-									<?php
-										
-										$sql="select * from payment";
-										$re = mysqli_query($con,$sql);
-										while($row = mysqli_fetch_array($re))
-										{
-										
-											$id = $row['id'];
-											
-											if($id % 2 ==1 )
-											{
-												echo"<tr class='gradeC'>
-													<td>".$row['id']." </td>
-													<td>".$row['title']." ".$row['fname']." ".$row['lname']."</td>
-													<td>".$row['cin']."</td>
-													<td>".$row['cout']."</td>
-													
-													
-													<td>$".$row['ttot']."</td>
-													<td>$".$row['mepr']."</td>
-													<td>$".$row['btot']."</td>
-													<td>$".$row['fintot']."</td>
-													<td>$".$row['fintot']*10/100 ."</td>
-													</tr>";
-											}
-											else
-											{
-												echo"<tr class='gradeU'>
-													<td>".$row['id']." </td>
-													<td>".$row['title']." ".$row['fname']." ".$row['lname']."</td>
-													
-													<td>".$row['cin']."</td>
-													<td>".$row['cout']."</td>
-													
-													
-													<td>$".$row['ttot']."</td>
-													<td>$".$row['mepr']."</td>
-													<td>$".$row['btot']."</td>
-													<td>$".$row['fintot']."</td>
-													<td>$".$row['fintot']*10/100 ."</td>
-													</tr>";
-											
-											}
-										
-										}
-										
-									?>
-                                        
-                                    </tbody>
-                                </table>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <!--End Advanced Tables -->
                 </div>
-            </div>
                 <!-- /. ROW  -->
-            
+
+
+                <div class="row">
+
+                    <?php
+                    //index.php
+                    //$connect = mysqli_connect("localhost", "root", "", "hotel");
+                    include('db.php');
+
+
+                    $query = "SELECT * FROM payment";
+                    $result = mysqli_query($con, $query);
+                    $chart_data = '';
+                    $tot = 0;
+                    while ($row = mysqli_fetch_array($result)) {
+                        $chart_data .= "{ date:'" . $row["cout"] . "', profit:" . $row["fintot"] * 10 / 100 . "}, ";
+                        $tot = $tot + $row["fintot"] * 10 / 100;
+                    }
+                    $chart_data = substr($chart_data, 0, -2);
+
+                    ?>
+
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <div id="chart"></div>
+                    <div class="col-md-12">
+                        <!-- Advanced Tables -->
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Check in</th>
+                                                <th>Check out</th>
+                                                <th>Room Rent</th>
+                                                <th>Bed Rent</th>
+                                                <th>Meals </th>
+                                                <th>Gr.Total</th>
+                                                <th>Profit</th>
+
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php
+
+                                            $sql = "select * from payment";
+                                            $re = mysqli_query($con, $sql);
+                                            while ($row = mysqli_fetch_array($re)) {
+
+                                                $id = $row['id'];
+
+                                                if ($id % 2 == 1) {
+                                                    echo "<tr class='gradeC'>
+													<td>" . $row['id'] . " </td>
+													<td>" . $row['title'] . " " . $row['fname'] . " " . $row['lname'] . "</td>
+													<td>" . $row['cin'] . "</td>
+													<td>" . $row['cout'] . "</td>
+													
+													
+													<td>$" . $row['ttot'] . "</td>
+													<td>$" . $row['mepr'] . "</td>
+													<td>$" . $row['btot'] . "</td>
+													<td>$" . $row['fintot'] . "</td>
+													<td>$" . $row['fintot'] * 10 / 100 . "</td>
+													</tr>";
+                                                } else {
+                                                    echo "<tr class='gradeU'>
+													<td>" . $row['id'] . " </td>
+													<td>" . $row['title'] . " " . $row['fname'] . " " . $row['lname'] . "</td>
+													
+													<td>" . $row['cin'] . "</td>
+													<td>" . $row['cout'] . "</td>
+													
+													
+													<td>$" . $row['ttot'] . "</td>
+													<td>$" . $row['mepr'] . "</td>
+													<td>$" . $row['btot'] . "</td>
+													<td>$" . $row['fintot'] . "</td>
+													<td>$" . $row['fintot'] * 10 / 100 . "</td>
+													</tr>";
+                                                }
+                                            }
+
+                                            ?>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!--End Advanced Tables -->
+                    </div>
                 </div>
-               
+                <!-- /. ROW  -->
+
             </div>
-        
-               
+
+        </div>
+
+
     </div>
-             <!-- /. PAGE INNER  -->
-            </div>
-         <!-- /. PAGE WRAPPER  -->
-     <!-- /. WRAPPER  -->
+    <!-- /. PAGE INNER  -->
+    </div>
+    <!-- /. PAGE WRAPPER  -->
+    <!-- /. WRAPPER  -->
     <!-- JS Scripts-->
     <!-- jQuery Js -->
     <script src="assets/js/jquery-1.10.2.js"></script>
-      <!-- Bootstrap Js -->
+    <!-- Bootstrap Js -->
     <script src="assets/js/bootstrap.min.js"></script>
     <!-- Metis Menu Js -->
     <script src="assets/js/jquery.metisMenu.js"></script>
-     <!-- DATA TABLE SCRIPTS -->
+    <!-- DATA TABLE SCRIPTS -->
     <script src="assets/js/dataTables/jquery.dataTables.js"></script>
     <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#dataTables-example').dataTable();
-            });
+    <script>
+        $(document).ready(function() {
+            $('#dataTables-example').dataTable();
+        });
     </script>
-         <!-- Custom Js -->
+    <!-- Custom Js -->
     <script src="assets/js/custom-scripts.js"></script>
-    
-   
+
+
 </body>
+
 </html>
 <script>
-Morris.Bar({
- element : 'chart',
- data:[<?php echo $chart_data; ?>],
- xkey:'date',
- ykeys:['profit'],
- labels:['Profit'],
- hideHover:'auto',
- stacked:true
-});
+    Morris.Bar({
+        element: 'chart',
+        data: [<?php echo $chart_data; ?>],
+        xkey: 'date',
+        ykeys: ['profit'],
+        labels: ['Profit'],
+        hideHover: 'auto',
+        stacked: true
+    });
 </script>
--- admin/settings.php
+++ admin/settings.php
@@ -1,25 +1,26 @@
-<?php  
-session_start();  
-if(!isset($_SESSION["user"]))
-{
- header("location:index.php");
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+    header("location:index.php");
 }
-?> 
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-      <meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SUNRISE HOTEL</title>
-	<!-- Bootstrap Styles-->
+    <title>KYLE VIEW CHARLETS</title>
+    <!-- Bootstrap Styles-->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
-     <!-- FontAwesome Styles-->
+    <!-- FontAwesome Styles-->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
-        <!-- Custom Styles-->
+    <!-- Custom Styles-->
     <link href="assets/css/custom-styles.css" rel="stylesheet" />
-     <!-- Google Fonts-->
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <!-- Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
+
 <body>
     <div id="wrapper">
         <nav class="navbar navbar-default top-navbar" role="navigation">
@@ -34,7 +35,7 @@ if(!isset($_SESSION["user"]))
             </div>
 
             <ul class="nav navbar-top-links navbar-right">
-			
+
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
@@ -48,7 +49,7 @@ if(!isset($_SESSION["user"]))
                         <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
                     </ul>
-					
+
                     <!-- /.dropdown-user -->
                 </li>
                 <!-- /.dropdown -->
@@ -62,130 +63,120 @@ if(!isset($_SESSION["user"]))
                     <li>
                         <a class="active-menu" href="settings.php"><i class="fa fa-dashboard"></i>Room Status</a>
                     </li>
-					<li>
-                        <a  href="room.php"><i class="fa fa-plus-circle"></i>Add Room</a>
+                    <li>
+                        <a href="room.php"><i class="fa fa-plus-circle"></i>Add Room</a>
                     </li>
                     <li>
-                        <a   href="roomdel.php"><i class="fa fa-pencil-square-o"></i> Delete Room</a>
+                        <a href="roomdel.php"><i class="fa fa-pencil-square-o"></i> Delete Room</a>
                     </li>
-					
 
-                    
+
+
             </div>
 
         </nav>
         <!-- /. NAV SIDE  -->
-       
-        <div id="page-wrapper" >
+
+        <div id="page-wrapper">
             <div id="page-inner">
-			 <div class="row">
+                <div class="row">
                     <div class="col-md-12">
                         <h1 class="page-header">
-                           Available <small> Rooms</small>
+                            Available <small> Rooms</small>
                         </h1>
                     </div>
-                </div> 
-                 
-                                 
-            <?php
-						include ('db.php');
-						$sql = "select * from room";
-						$re = mysqli_query($con,$sql)
-				?>
+                </div>
+
+
+                <?php
+                include('db.php');
+                $sql = "select * from room";
+                $re = mysqli_query($con, $sql)
+                ?>
                 <div class="row">
-				
-				
-				<?php
-										while($row= mysqli_fetch_array($re))
-										{
-												$id = $row['type'];
-											if($id == "Superior Room") 
-											{
-												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+
+
+                    <?php
+                    while ($row = mysqli_fetch_array($re)) {
+                        $id = $row['type'];
+                        if ($id == "Superior Room") {
+                            echo "<div class='col-md-3 col-sm-12 col-xs-12'>
 													<div class='panel panel-primary text-center no-boder bg-color-blue'>
 														<div class='panel-body'>
 															<i class='fa fa-users fa-5x'></i>
-															<h3>".$row['bedding']."</h3>
+															<h3>" . $row['bedding'] . "</h3>
 														</div>
 														<div class='panel-footer back-footer-blue'>
-															".$row['type']."
+															" . $row['type'] . "
 
 														</div>
 													</div>
 												</div>";
-											}
-											else if ($id == "Deluxe Room")
-											{
-												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+                        } else if ($id == "Deluxe Room") {
+                            echo "<div class='col-md-3 col-sm-12 col-xs-12'>
 													<div class='panel panel-primary text-center no-boder bg-color-green'>
 														<div class='panel-body'>
 															<i class='fa fa-users fa-5x'></i>
-															<h3>".$row['bedding']."</h3>
+															<h3>" . $row['bedding'] . "</h3>
 														</div>
 														<div class='panel-footer back-footer-green'>
-															".$row['type']."
+															" . $row['type'] . "
 
 														</div>
 													</div>
 												</div>";
-											
-											}
-											else if($id =="Guest House")
-											{
-												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+                        } else if ($id == "Guest House") {
+                            echo "<div class='col-md-3 col-sm-12 col-xs-12'>
 													<div class='panel panel-primary text-center no-boder bg-color-brown'>
 														<div class='panel-body'>
 															<i class='fa fa-users fa-5x'></i>
-															<h3>".$row['bedding']."</h3>
+															<h3>" . $row['bedding'] . "</h3>
 														</div>
 														<div class='panel-footer back-footer-brown'>
-															".$row['type']."
+															" . $row['type'] . "
 
 														</div>
 													</div>
 												</div>";
-											
-											}
-											else if($id =="Single Room")
-											{
-												echo"<div class='col-md-3 col-sm-12 col-xs-12'>
+                        } else if ($id == "Single Room") {
+                            echo "<div class='col-md-3 col-sm-12 col-xs-12'>
 													<div class='panel panel-primary text-center no-boder bg-color-red'>
 														<div class='panel-body'>
 															<i class='fa fa-users fa-5x'></i>
-															<h3>".$row['bedding']."</h3>
+															<h3>" . $row['bedding'] . "</h3>
 														</div>
 														<div class='panel-footer back-footer-red'>
-															".$row['type']."
+															" . $row['type'] . "
 
 														</div>
 													</div>
 												</div>";
-											
-											}
-										}
-									?>
-                    
+                        }
+                    }
+                    ?>
+
                 </div>
                 <!-- /. ROW  -->
-                
-                                
-                  
-            
-			 <!-- /. PAGE INNER  -->
+
+
+
+
+                <!-- /. PAGE INNER  -->
             </div>
-         <!-- /. PAGE WRAPPER  -->
+            <!-- /. PAGE WRAPPER  -->
         </div>
-     <!-- /. WRAPPER  -->
-    <!-- JS Scripts-->
-    <!-- jQuery Js -->
-    <script src="assets/js/jquery-1.10.2.js"></script>
-      <!-- Bootstrap Js -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- Metis Menu Js -->
-    <script src="assets/js/jquery.metisMenu.js"></script>
-      <!-- Custom Js -->
-    <script src="assets/js/custom-scripts.js"></script>
-    
-   
+        <!-- /. WRAPPER  -->
+        <!-- JS Scripts-->
+        <!-- jQuery Js -->
+        <script src="assets/js/jquery-1.10.2.js"></script>
+        <!-- Bootstrap Js -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- Metis Menu Js -->
+        <script src="assets/js/jquery.metisMenu.js"></script>
+        <!-- Custom Js -->
+        <script src="assets/js/custom-scripts.js"></script>
+
+
 </body>
+
 </html>
